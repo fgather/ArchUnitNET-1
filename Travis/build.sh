95e86998c738d4452467078f0f84f6f46cca450c
GeorgKreuzmayr <g.kreuzmayr@gmail.com>
2020-09-10 09:14:40 +0200

    Add Source to build script
    Signed-off-by: GeorgKreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/build.sh
+++ Travis/build.sh
@@ -1,9 +1,10 @@
 ﻿#!/usr/bin/env bash
 ApiKey=$1
+Source=$2
 
 set -ev
 dotnet restore
 dotnet build -c Release
 [[ -z "$TRAVIS_TAG" ]] && dotnet pack -c Release --output nupkgs -p:PackageVersion="0.0.0" -p:AssemblyVersion="0.0.0.0" || ([[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] && dotnet pack -c Release --output nupkgs -p:PackageVersion="$TRAVIS_TAG" -p:AssemblyVersion="$TRAVIS_TAG.0" || (echo "Git Tag has to resemble a package version (e.g. 1.0.0)." && exit 1 ))
 dotnet test -c Release
-dotnet nuget push ./ArchUnitNET/nupkgs/Georg.TestPackage.*.nupkg -k $ApiKey
+dotnet nuget push ./ArchUnitNET/nupkgs/Georg.TestPackage.*.nupkg -k $ApiKey $Source
