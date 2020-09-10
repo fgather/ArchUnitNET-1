262b56f47f8df51adb65c251827f9f49a83c89ba
GeorgKreuzmayr <g.kreuzmayr@gmail.com>
2020-09-10 09:20:29 +0200

    Add source in travis, use parameter
    Signed-off-by: GeorgKreuzmayr <g.kreuzmayr@gmail.com>

--- Travis/build.sh
+++ Travis/build.sh
@@ -7,4 +7,4 @@ dotnet restore
 dotnet build -c Release
 [[ -z "$TRAVIS_TAG" ]] && dotnet pack -c Release --output nupkgs -p:PackageVersion="0.0.0" -p:AssemblyVersion="0.0.0.0" || ([[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] && dotnet pack -c Release --output nupkgs -p:PackageVersion="$TRAVIS_TAG" -p:AssemblyVersion="$TRAVIS_TAG.0" || (echo "Git Tag has to resemble a package version (e.g. 1.0.0)." && exit 1 ))
 dotnet test -c Release
-dotnet nuget push ./ArchUnitNET/nupkgs/Georg.TestPackage.*.nupkg -k $ApiKey $Source
+dotnet nuget push ./ArchUnitNET/nupkgs/Georg.TestPackage.*.nupkg -k $ApiKey -s $Source
